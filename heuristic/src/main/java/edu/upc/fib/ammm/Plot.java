f174baba5fa62a98878c08f55e0f23b5a299d255
wsxarcher <marco.bartoli.dev@gmail.com>
2024-05-26 15:51:28 +0200

    better plot

--- heuristic/src/main/java/edu/upc/fib/ammm/Plot.java
+++ heuristic/src/main/java/edu/upc/fib/ammm/Plot.java
@@ -2,6 +2,7 @@ package edu.upc.fib.ammm;
 
 import edu.upc.fib.ammm.utils.Globals;
 import lombok.SneakyThrows;
+import org.knowm.xchart.CategoryChartBuilder;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.VectorGraphicsEncoder;
 
@@ -10,6 +11,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 import com.opencsv.CSVReader;
 import org.knowm.xchart.style.Styler;
@@ -19,13 +21,16 @@ public class Plot {
     @SneakyThrows
     public static void main(String[] args) {
         var timeChart = new XYChartBuilder().theme(Styler.ChartTheme.GGPlot2).width(800).height(800).title("Time Chart").xAxisTitle("N").yAxisTitle("Time").build();
-        var objectiveChart = new XYChartBuilder().theme(Styler.ChartTheme.GGPlot2).width(800).height(800).title("Objective Chart").xAxisTitle("N").yAxisTitle("Objective").build();
+        var objectiveChart = new CategoryChartBuilder().theme(Styler.ChartTheme.GGPlot2).width(800).height(800).title("Objective Chart").xAxisTitle("N").yAxisTitle("Objective").build();
 
         timeChart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideS);
         objectiveChart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideS);
 
         //timeChart.getStyler().setYAxisLogarithmic(true);
         //timeChart.setYAxisTitle("log(Time)");
+        //objectiveChart.getStyler().setOverlapped(true);
+        //objectiveChart.getStyler().setYAxisLogarithmic(true);
+        objectiveChart.getStyler().setXAxisMaxLabelCount(20);
 
         try (var reader = Files.newBufferedReader(Path.of(args[0]))) {
             try (CSVReader csvReader = new CSVReader(reader)) {
@@ -46,6 +51,14 @@ public class Plot {
                 }
 
                 for (var entry : all) {
+                    double bestObjective = 0.0;
+                    for (int i = 2; i < entry.length; i += 2) {
+                        var objective = Double.valueOf(entry[i+1]);
+                        if (objective > bestObjective) {
+                            bestObjective = objective;
+                        }
+                    }
+
                     ns.add(Double.valueOf(entry[1]));
                     for (int i = 2; i < entry.length; i += 2) {
                         var time = Double.valueOf(entry[i]);
@@ -70,7 +83,7 @@ public class Plot {
                 for (var algo: objectives.entrySet()) {
                     var series = objectiveChart.addSeries(algo.getKey(), ns, algo.getValue());
                     series.setMarker(SeriesMarkers.NONE);
-                    series.setSmooth(true);
+                    //series.setSmooth(true);
                 }
 
                 // To import in the report
